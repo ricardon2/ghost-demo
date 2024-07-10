4e6ccb4aff10b1a77a99bd53d4fa3de5ae4a3fd1
Ricardo Nascimento <2000119312@2000119312-m9v1w0cfc4.home>
2024-07-10 17:23:54 +0200

    Min req for starting development instance

--- IaC/terraform/appservice.tf
+++ IaC/terraform/appservice.tf
@@ -16,47 +16,38 @@ resource "azurerm_linux_web_app" "prod" {
         headers = []
         ip_address = null
         virtual_network_subnet_id = null
-      },
-      {
-        service_tag               = "AppService"
-        name                      = "AppServiceOnly"
-        description               = "Allow AppService and Functions apps"
-        priority                  = 300
-        action                    = "Allow"
-        headers = []
-        ip_address = null
-        virtual_network_subnet_id = null
       }
     ]
   }
 
   app_settings = {
     #Settings for ghost
-    database__client                    = "mysql"
-    database__connection__host          = "${azurerm_mysql_flexible_server.prod.name}.mysql.database.azure.com"
-    database__connection__port          = "3306"
-    database__connection__user          = "${var.mysql_administrator_login}@${azurerm_mysql_flexible_server.prod.name}"
-    database__connection__password      = "${var.mysql_administrator_login_password}"
-    database__connection__database      = "ghost"
-    database__connection__ssl           = "true"
-    WEBSITES_PORT                       = "2368"
-    WEBSITES_ENABLE_APP_SERVICE_STORAGE = "true"
-    NODE_ENV                            = "production"
-    url                                 = "https://ghost-FrontDoor.azurefd.net"
-    GHOST_CONTENT                       = "/var/lib/ghost/content_files/"
-    paths__contentPath	                = "/var/lib/ghost/content_files/"
-    privacy__useUpdateCheck	            = "false"
+    # database__client                            = "mysql"
+    # database__connection__host                  = "${azurerm_mysql_flexible_server.prod.name}.mysql.database.azure.com"
+    # database__connection__port                  = "3306"
+    # database__connection__user                  = "${var.mysql_administrator_login}@${azurerm_mysql_flexible_server.prod.name}"
+    # database__connection__password              = "${var.mysql_administrator_login_password}"
+    # database__connection__database              = "ghost"
+    # database__connection__ssl                   = "true"
+    # database__connection__ssl_minVersion        = "TLSv1.2"
+    WEBSITES_PORT                               = "2368"
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE         = "true"
+    NODE_ENV                                    = "development"
+    url                                         = "https://ghost-FrontDoor.azurefd.net"
+    GHOST_CONTENT                               = "/var/lib/ghost/content_files/"
+    paths__contentPath	                        = "/var/lib/ghost/content_files/"
+    privacy__useUpdateCheck	                    = "false"
     #Settings for private Container Registries
-    DOCKER_REGISTRY_SERVER_URL      = "https://${azurerm_container_registry.acr_active.login_server}"
-    DOCKER_REGISTRY_SERVER_USERNAME = "${azurerm_container_registry.acr_active.admin_username}"
-    DOCKER_REGISTRY_SERVER_PASSWORD = "${azurerm_container_registry.acr_active.admin_password}"
+    DOCKER_REGISTRY_SERVER_URL                  = "https://${azurerm_container_registry.acr_active.login_server}"
+    DOCKER_REGISTRY_SERVER_USERNAME             = "${azurerm_container_registry.acr_active.admin_username}"
+    DOCKER_REGISTRY_SERVER_PASSWORD             = "${azurerm_container_registry.acr_active.admin_password}"
     #Settings for application insights
     APPINSIGHTS_INSTRUMENTATIONKEY              = "${azurerm_application_insights.ai_appservice_active.instrumentation_key}"
     APPLICATIONINSIGHTS_CONNECTION_STRING       = "${azurerm_application_insights.ai_appservice_active.connection_string}"
     ApplicationInsightsAgent_EXTENSION_VERSION  = "~2"
   }
 
-  storage_account {
+    storage_account {
     name          = "ContentBlobVolume"
     type          = "AzureBlob"
     account_name  = "${azurerm_storage_account.prod.name}"
--- IaC/terraform/mysqldatabase.tf
+++ IaC/terraform/mysqldatabase.tf
@@ -1,7 +1,7 @@
-resource "azurerm_mysql_flexible_database" "mysql_database_prod" {
-  name                = "ghost"
-  resource_group_name = data.azurerm_resource_group.active_rg.name
-  server_name         = azurerm_mysql_flexible_server.prod.name
-  charset             = "utf8"
-  collation           = "utf8_unicode_ci"
-}
+# resource "azurerm_mysql_flexible_database" "mysql_database_prod" {
+#   name                = "ghost"
+#   resource_group_name = data.azurerm_resource_group.active_rg.name
+#   server_name         = azurerm_mysql_flexible_server.prod.name
+#   charset             = "utf8"
+#   collation           = "utf8_unicode_ci"
+# }
